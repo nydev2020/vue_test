c685fd717df6884cc0aa743e2e61af168ebe4477
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-03-14 16:31:10 +0900

    slotを使ったコンポーネント
    HTMLを送った

---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <slot></slot>
+    <slot></slot>
+    <p>{{ headerText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["headerText"]
+};
+</script>
---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>いいね({{ halfNumber }})</p>
+    <button v-on:click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  //親から子へデータを渡す時はpropsを使う
+  props: {
+    totalNumber: {
+      type: Number,
+      require: true,
+    }
+  },
+  computed: {
+    halfNumber: function() {
+      return this.totalNumber / 2
+    }
+  },
+  
+  methods: {
+    increment: function() {
+      //子側からの送り口
+      this.$emit("my-click", "this.totalNumber + 1");
+    }
+  },
+};
+</script>
+
+<style scoped>
+  div {
+    border: 5px solid red;
+  }
+</style>

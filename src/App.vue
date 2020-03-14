dc605e1b37aafcec1c1bcea667c885f675bc909c
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-03-14 19:55:36 +0900

    keep-alive
    動的コンポーネント
    keep-aliveタグで囲む
    ライフルサイクルフックを起こしたいならdeacivatedかactivatedを用いること

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,55 @@
 <template>
   <div>
     <LikeHeader>
-      <h4>腹へった</h4>
-      <p>デフォ</p>
-
-      <template v-slot:title="slotProps">
+      <template>
         <h1>トータルのいいね数</h1>
-        <h2>{{ slotProps }}</h2>
       </template>
       <template v-slot:number>
         <h2>{{ number }}</h2>
       </template>
     </LikeHeader>
     <LikeNumber v-bind:total-number="number" v-on:my-click="$event"></LikeNumber>
-    <LikeNumber v-bind:total-number="number"></LikeNumber>
+
+    <button v-on:click="currentComponent = 'Home'">Home</button>
+    <button v-on:click="currentComponent = 'About'">About</button>
+    
+    <!-- 動的コンポーネントの情報をキャッシュする -->
+    <keep-alive>
+      <!-- 動的コンポーネント -->
+      <component v-bind:is="currentComponent"></component>
+    </keep-alive>
+    
+    <!-- <About v-if="currentComponent === 'About'"></About>
+    <Home v-if="currentComponent === 'Home'"></Home> -->
 
   </div>
 </template>
 
 <script>
 //コンポーネントのローカル登録
-import LikeHeader from "./components/LikeHeader.vue"
+import LikeHeader from "./components/LikeHeader.vue";
 // import LikeNumber from "./components/LikeNumber.vue"
+
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
 
 export default {
   data: function() {
     return {
-      number: 18
+      number: 18,
+      //動的コンポーネントの部品
+      currentComponent: "Home",
     };
   },
+
   //コンポーネントのローカル登録
   components: {
     //keyとvalueが同一の時は短縮できる ES6の記法
     LikeHeader,
+    About,
+    Home,
     // LikeHeader: LikeHeader,
     // LikeNumber,
-
   }
 }
 </script>

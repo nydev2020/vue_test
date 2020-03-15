6d8e0e37a9bc57539dbee3fdbd7784e17e06f2f0
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-03-15 12:57:31 +0900

    directiveのローカル登録

--- src/main.js
+++ src/main.js
@@ -6,24 +6,22 @@ import LikeNumber from "./components/LikeNumber"
 Vue.config.productionTip = false
 //コンポーネントのグローバル登録
 Vue.component('LikeNumber', LikeNumber);
+
 //カスタムディレクティブのグローバル登録
-Vue.directive("border", {
-  bind(el, binding, vnode) {
+// Vue.directive("border", {
+  // bind(el, binding, vnode) {
     // ディレクティブが初めて対象の要素に紐づいたとき
-  },
-  inserted() {
-    // 親NODEに挿入された時
-  },
-  update() {
+  // },
+  // update(el, binding, vnode) {
     // コンポーネントが更新される度、子コンポーネントが更新される前
-  },
-  componentUpdated() {
-    // コンポーネントが更新される度、子コンポーンとが更新された後
-  },
-  unbind() {
-    // ディレクティブが紐付いている要素から取り除かれた時
-  }, 
-});
+  // },
+// });
+  
+// bindとupdateは頻出しやすいので省略記法が用意されている
+// Vue.directive("border", function(el, binding) {
+//   el.style.border = "solid black 2px";
+//   el.style.borderWidth = binding.value;
+// });
 
 new Vue({
   render: h => h(App),
